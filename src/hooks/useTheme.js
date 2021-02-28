7019aa190f87f606cb145e4033d8246548a8c38d
Was War <51692891+0ww0@users.noreply.github.com>
2021-02-28 17:44:46 +0800

    dsdsdw

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -6,9 +6,14 @@ const useTheme = () => {
     	const stored = false;
 	if (typeof localStorage !== `undefined`)  {
 		const stored = localStorage.getItem("darkmode");
-		
-	} 
+	}
 	const [mode, setmode] = useState(stored === "true" ? true : false);
+	useEffect(() => {
+	    if (stored) {
+	      setmode(stored);
+	    }
+	  }, []);
+	
     const theme = mode ? DarkTheme : LightTheme;
 	
     const toggleMode = () => {
